8292ecad7e3069c941e9daaac453150c901bdf5b
madelinenlee <madelinenlee@gmail.com>
2019-01-25 18:42:01 -0500

    Update OPIM_241_Quiz_1_analysis.py

--- OPIM_241_Quiz_1_analysis.py
+++ OPIM_241_Quiz_1_analysis.py
@@ -17,3 +17,21 @@ for attribute in attributes:
     print(boston_housing_data[attribute].describe())
 
     
+def missing_values(data_frame):
+    attributes = data_frame.columns.tolist()
+    final_missing = pd.DataFrame(columns = attributes)
+    for item in attributes:
+        missing_data = data_frame[data_frame[item].isnull() == True]
+        final_missing = final_missing.append(missing_data)
+        final_missing = final_missing.drop_duplicates()
+    print("number of rows with missing values: " + str(final_missing.shape[0]))
+   
+    return (final_missing)
+
+boston_housing_missing = missing_values(boston_housing_data)
+
+for item in attributes:
+    boston_housing_data[item].hist()
+    plt.title(item)
+    plt.savefig(item+'_hist.png')
+    plt.show()
